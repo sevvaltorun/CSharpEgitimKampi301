f52dda05be41c5b26a151e5da6e9ee286568de98
sevvaltorun <sevvaltrn3@gmail.com>
2024-12-02 15:07:12 +0300

    Add project files.

---
+++ EgitimKampi301.DataAccessLayer/Context/CampContext.cs
@@ -0,0 +1,23 @@
+﻿using EgitimKampi301.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EgitimKampi301.DataAccessLayer.Context
+{
+    public class CampContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+
+
+    }
+}
